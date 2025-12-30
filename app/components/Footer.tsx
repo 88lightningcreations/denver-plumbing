5778925e27cac380628cf94779898b5207f61a1b
88coderz <88lightningcreations@gmail.com>
2025-12-30 16:58:29 +0000

    added email

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -13,14 +13,15 @@ const Footer: React.FC = () => {
         <div className={styles.footerColumn}>
           <h3 className={styles.footerColumnTitle}>Contact Us</h3>
           <p className={styles.footerText}>4210 South Galapago Street, Englewood, CO 80110</p>
+          <p className={styles.footerText}>Email: PTP5280@gmail.com</p>
           <p className={styles.footerText}>Email: coloradoplumbingsolutions@gmail.com</p>
           <p className={styles.footerText}>Phone: (720) 298-1900</p>
         </div>
         <div className={styles.footerColumn}>
           <h3 className={styles.footerColumnTitle}>Stay Connected</h3>
           <div className={styles.socialIcons}>
-            <a href="https://www.facebook.com/people/Colorado-Plumbing-Solutions/61570501647326/#" className={styles.socialIcon}><FaFacebook /></a>
-            <a href="https://www.yelp.com/biz/peach-tree-plumbing-englewood-2?osq=denver+plumbing+team" className={styles.socialIcon}><FaYelp /></a>
+            <a href="https.www.facebook.com/people/Colorado-Plumbing-Solutions/61570501647326/#" className={styles.socialIcon} target="_blank" rel="noopener noreferrer"><FaFacebook /></a>
+            <a href="https://www.yelp.com/biz/peach-tree-plumbing-englewood-2?osq=denver+plumbing+team" className={styles.socialIcon} target="_blank" rel="noopener noreferrer"><FaYelp /></a>
             <a href="#" className={styles.socialIcon}><FaInstagram /></a>
           </div>
         </div>
